9eeb6aea686d5e03c89964636640de9e4230537b
Eten-Ekata <ekataelishaeten@gmail.com>
2023-05-09 01:46:24 +0100

    added posts slice and list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import PostsList from './features/posts/PostsList'
 import './index.css'
 
 function App() {
 
   return (
     <main className='App'>
-
+      <PostsList/>
     </main>
   )
 }
