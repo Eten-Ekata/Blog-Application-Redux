bad8d6ff2aa6285f24ea7b60ad5d3b5039ccc858
Eten-Ekata <ekataelishaeten@gmail.com>
2023-05-16 00:08:59 +0100

    added date feature

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,10 +1,12 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import {sub} from 'date-fns'
 
 const initialState = [
     {
         id: '1',
         title: 'Learning Redux Toolkit',
         content: "I've heard good things.",
+        date:sub(new date(),{minutes:10}).toISOString(),
         // date: sub(new Date(), { minutes: 10 }).toISOString(),
         // reactions: {
         //     thumbsUp: 0,
@@ -18,6 +20,7 @@ const initialState = [
         id: '2',
         title: 'Slices...',
         content: "The more I say slice, the more I want pizza.",
+        date:sub(new date(),{minutes:5}).toISOString(),
         // date: sub(new Date(), { minutes: 5 }).toISOString(),
         // reactions: {
         //     thumbsUp: 0,
@@ -43,6 +46,7 @@ const postsSlice = createSlice({
                     id:nanoid(),
                     title,
                     content,
+                    date: new Date().toISOString(),
                     userId
                 }
             }
