b3334e748957b2a8266695527f25e110feafff95
Eten-Ekata <ekataelishaeten@gmail.com>
2023-05-15 10:29:00 +0100

    added postform and users

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import PostsList from './features/posts/PostsList'
+import AddPostForm from './features/posts/AddPostForm'
 import './index.css'
 
 function App() {
 
   return (
     <main className='App'>
+      <AddPostForm/>
       <PostsList/>
     </main>
   )
