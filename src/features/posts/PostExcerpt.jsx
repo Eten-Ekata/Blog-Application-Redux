9dc237de3435f56fc16a55797dcd05e98f6f450d
Eten-Ekata <ekataelishaeten@gmail.com>
2023-06-02 13:28:58 +0100

    added new post form reducer

--- src/features/posts/PostExcerpt.jsx
+++ src/features/posts/PostExcerpt.jsx
@@ -5,7 +5,7 @@ import ReactionButtons from './ReactionButtons'
 const PostExcerpt = ({post}) => {
   return (
     <article>
-          <h3>{post.id}</h3>
+          <h3>{post.title}</h3>
           <p>{post.body.substring(0,100)}</p>
           <p className='postCredit'>
             <PostAuthor userId={post.userId}/>
